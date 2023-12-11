7196078948d602127cfb6d500a0512620d1a0d03
Himanshu200102 <himanshun01@arizona.edu>
2023-12-11 01:19:18 -0700

    Fetching And Displaying on the GUI

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
 import streamlit as st
-
-
-
-
-
+import pandas as pd
+from twitter_api_test import save_tweets
+from twitter_api_test import scraper
 st.set_page_config(page_title="Disaster Tweet Management")
 
 def main_page(keyword):
-
+    tweet = pd.read_csv("saved_tweets.csv")
     st.markdown("<center><h1>🕊️ Tweet Analytics for Disaster & Calamity Management</h1></center>", unsafe_allow_html=True)
 
-    st.write(keyword)
+
+    for user in tweet['user'].unique():
+        user_texts = tweet[tweet['user'] == user]['text']
+        user_link = tweet[tweet['user'] == user]['link']
+        with st.container(border=True):
+            col1, col2, col3 = st.columns(3)
+
+            with col1:
+                st.subheader("User")
+                st.write(user)
+
+            with col2:
+                st.subheader("Tweet")
+                for text in user_texts:
+                    st.write(text)
+
+            with col3:
+                st.subheader("Link")
+                for link in user_link:
+                    st.write(link)
 
 
 def sidebar_page():
@@ -23,7 +40,10 @@ def sidebar_page():
         btn = st.button("search")
 
         if btn:
+            tweets = scraper.get_tweets(terms=keyword, mode='hashtag', number=10, near='usa', exclude=['nativeretweets'], language='en')
+            save_tweets(tweets, topic=keyword)
             return keyword
+
 
 
 if __name__ == "__main__":
